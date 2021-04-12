a24d3b78a27d66157e454fa0c4395b7dcb85526f
iana <litvinova.iana1212@gmail.com>
2021-04-12 11:13:17 +0700

    feat: add 10-12 tasks

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let res = '';
+  for (let i = 0; i < str.length; i++) {
+    const index = res.indexOf(str[i]);
+    if (index > -1) {
+      if (str[i] === str[i - 1]) {
+        if (Number(res[index - 1] >= 2)) {
+          res = `${res.substring(0, index - 1)}${Number(res[index - 1]) + 1}${str[i]}`;
+        } else res = `${res.substring(0, index)}2${str[i]}`;
+      } else res = `${res}${str[i]}`;
+    } else res = `${res}${str[i]}`;
+  }
+  return res;
 }
-
 module.exports = encodeLine;
--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,29 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const res = {};
+  domains.forEach((domain) => {
+    const domainArray = domain.split('.').reverse();
+    let prev = '';
+    domainArray.forEach((el) => {
+      if (Object.keys(res).length > 0) {
+        const curr = `${prev}.${el}`;
+        if (Object.keys(res).find((key) => key === curr) !== undefined) {
+          res[`${curr}`] += 1;
+          prev = curr;
+        } else {
+          res[`${curr}`] = 1;
+          prev = curr;
+        }
+        return res;
+      }
+      res[`.${el}`] = 1;
+      prev = `.${el}`;
+      return res;
+    });
+  });
+  return res;
 }
 
 module.exports = getDNSStats;
